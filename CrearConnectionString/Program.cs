ff72e0f95645168862a5694d0b3fb3a3f51010cb
MiguelJA99 <165223398+MiguelJA99@users.noreply.github.com>
2024-04-09 13:48:17 -0600

    Respaldo de archivos

---
+++ CrearConnectionString/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrearConnectionString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+
+            SqlConnectionStringBuilder connectionStringBuilder = new();
+            connectionStringBuilder.DataSource = "JAMX-LP-055";
+            connectionStringBuilder.InitialCatalog = "Northwind_Mart";
+            connectionStringBuilder.IntegratedSecurity = true;
+            var cs= connectionStringBuilder.ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(cs))
+            {
+                connection.Open();
+                SqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "select * from Dim_Cliente";
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetString(1));
+                }
+                reader.Close();
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
